48b2a4135d7ca2d0ef1a9bed300867d65b018b83
hindblgcm <hind.belgacem@hotmail.com>
2025-01-11 15:05:54 +0100

    Refactor CSS file structure and update stylesheet links

--- index.php
+++ index.php
@@ -198,7 +198,7 @@ function displayPlayWithClouds($play, $playerPos, $mousePos)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <title>Jeu du chat et de la souris</title>
 </head>
 
